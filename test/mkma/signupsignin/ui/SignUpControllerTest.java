0492f59dbfdc513e13f0f789f7246df21da582c7
tinit <tinit@DESKTOP-FLNKDM9>
2020-11-01 16:54:37 +0100

    SignUp Test Class with most methods done.

--- test/mkma/signupsignin/ui/SignUpControllerTest.java
+++ test/mkma/signupsignin/ui/SignUpControllerTest.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mkma.signupsignin.ui;
 
-import java.util.concurrent.TimeoutException;
 import javafx.stage.Stage;
 import mkma.signupsignin.application.App;
-import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -22,7 +15,7 @@ import static org.testfx.matcher.control.TextInputControlMatchers.hasText;
 /**
  * TestFX test class for SignUpController.
  *
- * @author 2dam
+ * @author Martin Valietne Ainz
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SignUpControllerTest extends ApplicationTest {
@@ -35,12 +28,76 @@ public class SignUpControllerTest extends ApplicationTest {
     @Override
     public void stop() {
     }
-
+    
+    /**
+     * This method will verify the stage on lauch.
+     * TextBoxes will be empty by default.
+     * SignUpButton will be disabled as default.
+     * Back button will be enabled by default.
+     */
     @Test
-    public void UserTooShortTest() {
+    public void testA_initialstate() {
+        verifyThat("#txtUser", hasText(""));
+        verifyThat("#txtPass", hasText(""));
+        verifyThat("#txtPassAgain", hasText(""));
+        verifyThat("#txtEmail", hasText(""));
+        verifyThat("#txtName", hasText(""));
+        verifyThat("#btnSignUp", isDisabled());
+        verifyThat("#btnBack", isEnabled());
+    }
+    
+    /**
+     * This method will verify that SignUpButton is only enabled when all fields contain text.
+     * If there is at least one empty field SignUpButton should be disabled.
+     */
+    @Test
+    public void testB_SignUpButtonEnabled() {
+        
         clickOn("#txtUser");
-        write("username");
+        write("asd");
+        verifyThat("#btnSignUp", isDisabled());
+        
+        clickOn("#txtPass");
+        write("asd");
+        verifyThat("#btnSignUp", isDisabled());
+        
+        clickOn("#txtPassAgain");
+        write("asd");
+        verifyThat("#btnSignUp", isDisabled());
+        
+        clickOn("#txtEmail");
+        write("asd");
+        verifyThat("#btnSignUp", isDisabled());
+        
+        clickOn("#txtName");
+        write("asd");
+        verifyThat("btnSignUp", isEnabled());
 
+    }
+    
+    /**
+     * This method will verify that once pushed the button Sign Up changes to Signed Up.
+     */
+    public void testC_SignUpButtonChanged(){
+        clickOn("#txtUser");
+        write("Alberto");
+        clickOn("#txtPass");
+        write("Alberto!Garcia8");
+        clickOn("#txtPassAgain");
+        write("Alberto!Garcia8");
+        clickOn("#txtEmail");
+        write("albertogarica@gmail.com");
+        clickOn("#txtName");
+        write("Alberto García");
+        clickOn("#btnSignUp");
+        verifyThat("#btnSignUp", hasText("Signed Up"));
+    }
+    /**
+     * This method will verify that btnBack opens Sign In window and closes Sign Up window.
+     */
+    public void testD_btnBackOK(){
+        clickOn("#btnBack");
+        // verifyThat("#stage",  );
     }
 
 }
